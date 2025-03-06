766fe8e7a75b9b02b3f785f4c7994bd58c4279a7
Musa Musa Kannike <156730039+musamusakannike@users.noreply.github.com>
2025-03-06 18:52:53 +0100

    Added apk file

--- app/page.tsx
+++ app/page.tsx
@@ -303,10 +303,10 @@ export default function LandingPage() {
                 <div className="space-y-4">
                   <div className="flex flex-col gap-2 min-[400px]:flex-row">
                     <Button size="lg" className="bg-[#F5B800] text-[#082F14] hover:bg-[#FFD95B]" asChild>
-                      <Link href="#android-download">
+                      <a href="/proffyemphy-ideal-academy.apk">
                         <Download className="mr-2 h-4 w-4" />
                         Download APK (Android)
-                      </Link>
+                      </a>
                     </Button>
                     <Button
                       size="lg"
